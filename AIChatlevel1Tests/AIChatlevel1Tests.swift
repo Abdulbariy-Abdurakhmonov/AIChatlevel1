52d1056ec725c1b63a713cd30bde8ffbf6240f80
Abdulbariy-Abdurakhmonov <abdulboriy21business@gmail.com>
2026-03-13 04:16:43 +0300

    Initial Commit

---
+++ AIChatlevel1Tests/AIChatlevel1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatlevel1Tests.swift
+//  AIChatlevel1Tests
+//
+//  Created by Abdulboriy on 13/03/26.
+//
+
+import Testing
+@testable import AIChatlevel1
+
+struct AIChatlevel1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
