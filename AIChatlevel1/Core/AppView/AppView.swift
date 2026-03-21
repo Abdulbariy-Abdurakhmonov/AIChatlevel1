f211969380ff248b32072cf4e1d16ab80d622488
Abdulbariy-Abdurakhmonov <199811299+Abdulbariy-Abdurakhmonov@users.noreply.github.com>
2026-03-21 21:08:26 +0300

    [Feature] add AppView, AppViewBuilder

---
+++ AIChatlevel1/Core/AppView/AppView.swift
@@ -0,0 +1,57 @@
+//
+//  AppView.swift
+//  AIChatlevel1
+//
+//  Created by Abdulboriy on 21/03/26.
+//
+
+import SwiftUI
+
+struct AppViewBuilder<TabbarView: View, OnboardingView: View>: View {
+    
+    var showTapBar: Bool = false
+    @ViewBuilder var tabbarView: TabbarView
+    @ViewBuilder var onboardingView: OnboardingView
+    
+    var body: some View {
+        ZStack {
+            if showTapBar {
+                tabbarView
+                    .transition(.move(edge: .trailing))
+            } else {
+                onboardingView
+                    .transition(.move(edge: .leading))
+            }
+        }
+        .animation(.smooth, value: showTapBar)
+    }
+}
+
+struct AppView: View {
+    @State private var showTapBar: Bool = true
+    
+    var body: some View {
+        AppViewBuilder(
+            showTapBar: showTapBar,
+            tabbarView: {
+                ZStack {
+                    Color.blue.ignoresSafeArea()
+                    Text("Tab Bar")
+                }
+            },
+            onboardingView: {
+                ZStack {
+                    Color.red.ignoresSafeArea()
+                    Text("Onboarding")
+                }
+            }
+        )
+        .onTapGesture {
+            showTapBar.toggle()
+        }
+    }
+}
+
+#Preview {
+    AppView()
+}
---
+++ AIChatlevel1/Core/Root/AppView.swift
@@ -0,0 +1,18 @@
+//
+//  AppView.swift
+//  AIChatlevel1
+//
+//  Created by Abdulboriy on 21/03/26.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppView()
+}
