fb895d7b4bdc31237fb0b29ed8c1bd1a8572791c
Abdulbariy-Abdurakhmonov <199811299+Abdulbariy-Abdurakhmonov@users.noreply.github.com>
2026-03-29 22:08:39 +0300

    [Feature] Add welcomeView UI

---
+++ AIChatlevel1/Components/Images/ImageLoaderView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageLoaderView.swift
+//  AIChatlevel1
+//
+//  Created by Abdulboriy on 29/03/26.
+//
+
+import SwiftUI
+
+struct ImageLoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+}
---
+++ AIChatlevel1/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  AIChatlevel1
+//
+//  Created by Abdulboriy on 29/03/26.
+//
+
+import Foundation
