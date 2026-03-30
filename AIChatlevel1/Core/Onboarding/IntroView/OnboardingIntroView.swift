0d8cc58c9264ffb161696c8bf0558f09c7d1a5a8
Abdulbariy-Abdurakhmonov <199811299+Abdulbariy-Abdurakhmonov@users.noreply.github.com>
2026-03-30 07:51:33 +0300

    [Feature] Add Onboarding IntroView

---
+++ AIChatlevel1/Core/Onboarding/IntroView/OnboardingIntroView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingIntroView.swift
+//  AIChatlevel1
+//
+//  Created by Abdulboriy on 30/03/26.
+//
+
+import SwiftUI
+
+struct OnboardingIntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingIntroView()
+}
