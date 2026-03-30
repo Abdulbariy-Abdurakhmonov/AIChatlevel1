84d8c54bccb4056c9624d6bc9fa527fced36b16f
Abdulbariy-Abdurakhmonov <199811299+Abdulbariy-Abdurakhmonov@users.noreply.github.com>
2026-03-30 12:27:45 +0300

    [Feature] Add OnboardingView

---
+++ AIChatlevel1/Core/Onboarding/ColorView/OnboardingColorView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingColorView.swift
+//  AIChatlevel1
+//
+//  Created by Abdulboriy on 30/03/26.
+//
+
+import SwiftUI
+
+struct OnboardingColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingColorView()
+}
