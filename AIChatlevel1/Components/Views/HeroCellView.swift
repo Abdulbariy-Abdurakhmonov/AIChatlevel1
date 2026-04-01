09bf1d8fb14a4d4c32824ff094dfe0dfb97576f9
Abdulbariy-Abdurakhmonov <199811299+Abdulbariy-Abdurakhmonov@users.noreply.github.com>
2026-04-01 12:11:17 +0300

    [Feature] Add HeroCellView

---
+++ AIChatlevel1/Components/Views/HeroCellView.swift
@@ -0,0 +1,18 @@
+//
+//  HeroCellView.swift
+//  AIChatlevel1
+//
+//  Created by Abdulboriy on 01/04/26.
+//
+
+import SwiftUI
+
+struct HeroCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeroCellView()
+}
