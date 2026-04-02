3bd170d90224c6ea3a300d725f79ec2860e7c7f4
Abdulbariy-Abdurakhmonov <199811299+Abdulbariy-Abdurakhmonov@users.noreply.github.com>
2026-04-02 12:35:24 +0300

    [Feature] Make carouselView generic and reusable

--- AIChatlevel1/Components/Views/CarouselView.swift
+++ AIChatlevel1/Components/Views/CarouselView.swift
@@ -7,27 +7,24 @@
 
 import SwiftUI
 
-struct CarouselView: View {
+struct CarouselView<Content: View, T: Hashable>: View {
 
-    var items: [AvatarModel] = AvatarModel.mocks
-    @State private var selection: AvatarModel?
+    var items: [T]
+    @ViewBuilder var content: (T) -> Content
+    @State private var selection: T?
 
     var body: some View {
         VStack(spacing: 12) {
             ScrollView(.horizontal) {
                 LazyHStack(spacing: 0) {
                     ForEach(items, id: \.self) { item in
-                        HeroCellView(
-                            title: item.name,
-                            subtitle: item.characterDescription,
-                            imageName: item.profileImageName
-                        )
-                        .scrollTransition(.interactive.threshold(.visible(0.95)), transition: { content, phase in
-                            content
-                                .scaleEffect(phase.isIdentity ? 1 : 0.9)
-                        })
-                        .containerRelativeFrame(.horizontal, alignment: .center)
-                        .id(item)
+                        content(item)
+                            .scrollTransition(.interactive.threshold(.visible(0.95)), transition: { content, phase in
+                                content
+                                    .scaleEffect(phase.isIdentity ? 1 : 0.9)
+                            })
+                            .containerRelativeFrame(.horizontal, alignment: .center)
+                            .id(item)
                     }
                 }
             }
@@ -62,6 +59,14 @@ struct CarouselView: View {
 }
 
 #Preview {
-    CarouselView()
-        .padding()
+    CarouselView(
+        items: AvatarModel.mocks,
+        content: { item in
+            HeroCellView(
+                title: item.name,
+                subtitle: item.characterDescription,
+                imageName: item.profileImageName
+            )
+        })
+    .padding()
 }
