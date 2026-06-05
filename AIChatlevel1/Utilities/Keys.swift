5cd6517c7a26c08e228abd343e8519726b3961f9
Abdulbariy-Abdurakhmonov <199811299+Abdulbariy-Abdurakhmonov@users.noreply.github.com>
2026-06-06 01:16:41 +0500

    [FIx] secret keys has been added to info Plist

--- AIChatlevel1/Utilities/Keys.swift
+++ AIChatlevel1/Utilities/Keys.swift
@@ -8,8 +8,7 @@
 import Foundation
 
 struct Keys {
-    static let openAI = "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"
+
+    static let openAI = Bundle.main.infoDictionary?["OPENAI_API_KEY"] as? String ?? ""
 }
 
-
-// "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"
